3a6a7a7a98a88eb87747d49cd001a6d4cc22df56
Junsoo <73781220+junsoo37@users.noreply.github.com>
2021-06-28 02:23:24 +0900

    [ADD] content_based.py
    ADD content based recommendation system using tmdb_5000movies

---
+++ content_based.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import warnings
+from ast import literal_eval
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+warnings.filterwarnings('ignore')
+
+
+class ContentBasedRecommendation:
+    def __init__(self, movie_data, top_n):
+        self.movie_data = movie_data
+        self.top_n = top_n
+
+    def data_preprocessing(self):
+        self.movie_data['genres'] = self.movie_data['genres'].apply(literal_eval)
+        self.movie_data['keywords'] = self.movie_data['keywords'].apply(literal_eval)
+        self.movie_data['genres'] = self.movie_data['genres'].apply(lambda x: [y['name'] for y in x])
+        self.movie_data['keywords'] = self.movie_data['keywords'].apply(lambda x: [y['name'] for y in x])
+        return
+
+    def vectorize_genres(self):
+        self.movie_data['genres_str'] = self.movie_data['genres'].apply(lambda x: ' '.join(x))
+        genre_vectorizer = CountVectorizer()
+        gerne_matrix = genre_vectorizer.fit_transform(self.movie_data['genres_str'])
+        genre_sim = cosine_similarity(gerne_matrix, gerne_matrix)
+        sorted_genre_sim = genre_sim.argsort()[:, ::-1]
+        return sorted_genre_sim
+
+    def get_sim_movies(self, sorted_idx, title):
+        title_movie = self.movie_data[self.movie_data['title'] == title]
+        title_idx = title_movie.index.values
+        similar_indices = sorted_idx[title_idx, :(self.top_n*2)]
+        similar_indices = similar_indices.reshape(-1)
+        similar_indices = similar_indices[similar_indices != title_idx]
+        return self.movie_data.iloc[similar_indices].sort_values('weighted_vote', ascending=False)[:self.top_n]
+
+    def cal_weighted_vote_average(self, info):
+        m = self.movie_data['vote_count'].quantile(0.6)
+        C = self.movie_data['vote_average'].mean()
+        v = info['vote_count']
+        R = info['vote_average']
+        return (v/(v+m) * R + (m/(m+v)) * C)
+
+    def run(self):
+        self.movie_data = self.movie_data[['genres', 'id', 'keywords', 'popularity', 'title', 'vote_average', 'vote_count']]
+        self.data_preprocessing()
+        self.movie_data['weighted_vote'] = self.movie_data.apply(lambda x: self.cal_weighted_vote_average(x), axis=1)
+        genre_similarity = self.vectorize_genres()
+        recommend_movies = self.get_sim_movies(genre_similarity, 'The Godfather')
+        return recommend_movies[['title', 'vote_average', 'weighted_vote']]
+
+
+if __name__ == '__main__':
+    data = pd.read_csv('~/archive/tmdb_5000_movies.csv')
+    recommendation_system = ContentBasedRecommendation(movie_data=data, top_n=7)
+    recommend_res = recommendation_system.run()
+    print(recommend_res)
